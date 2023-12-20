527d027c5ff932125c8f88e482478d70530bf164
hypertacos520 <dawsonthompson2000@gmail.com>
2023-12-20 16:31:07 -0500

    Better Termination Function

--- terminate.py
+++ terminate.py
@@ -1,6 +1,19 @@
 import system_calls as system
+import sys
 
-pid = system.load_data()
-print(pid)
+if __name__ == "__main__":
+    # Check if at least one command-line argument is provided
+    if len(sys.argv) < 2:
+        print("Usage: python terminate.py <game_name>")
+        exit(-1)
 
-system.terminate_process(pid)
+    # Get Game Name
+    # game_name = platform_api.get_app_name(sys.argv[1])
+    game_name = sys.argv[1]
+
+    pid = system.get_pid_by_name(game_name)
+    print(pid)
+    if not pid == None:
+        system.terminate_process(pid)
+    else:
+        print(f"No process found with name: '{game_name}'")
