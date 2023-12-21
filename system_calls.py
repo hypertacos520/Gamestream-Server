09fbe284d4da2a90640731bbc78005fde10da4f6
hypertacos520 <dawsonthompson2000@gmail.com>
2023-12-21 00:37:01 -0500

    Revert to previous version

--- system_calls.py
+++ system_calls.py
@@ -1,4 +1,4 @@
-import psutil, os, subprocess, platform, json, pyautogui
+import psutil, os, signal, subprocess, platform, json, pyautogui, time
 
 def get_pid_by_name(partial_name):
     words = partial_name.split()
@@ -20,33 +20,25 @@ def get_pid_by_name(partial_name):
 
 def terminate_process(pid):
     try:
-        process = psutil.Process(pid)
-        
-        # Send the terminate signal
-        process.terminate()
+        # Send the SIGTERM signal
+        os.kill(int(pid), signal.SIGTERM)
         print(f"Process with PID {pid} terminated successfully.")
-
-        # Wait for the process to terminate gracefully
-        process.wait(timeout=1)
-
-        # Check if the process is still alive after terminate
-        if process.is_running():
-            print(f"Process with PID {pid} is still alive. Forcefully terminating.")
-            
-            # Forcefully terminate the process
-            process.kill()
+        
+        # Check if the process is still alive after SIGTERM
+        time.sleep(1)  # Give some time for the process to respond to SIGTERM
+        try:
+            os.kill(int(pid), 0)  # Check if the process is still alive
+            print(f"Process with PID {pid} is still alive. Sending SIGKILL.")
+            os.kill(int(pid), signal.SIGKILL)  # Send the SIGKILL signal
             print(f"Process with PID {pid} forcefully terminated.")
-        else:
-            print(f"Process with PID {pid} terminated gracefully.")
-    except psutil.NoSuchProcess:
+        except ProcessLookupError:
+            print(f"Process with PID {pid} not found after SIGTERM.")
+    except ProcessLookupError:
         print(f"Error: Process with PID {pid} not found.")
-        exit(-1)
-    except psutil.AccessDenied:
+    except PermissionError:
         print(f"Error: Permission denied to terminate process with PID {pid}.")
-        exit(-2)
-    except Exception as e:
-        print(f"Error: Process {pid} failed to terminate. {str(e)}")
-        exit(-3)
+    except:
+        print(f"Error: Process {pid} failed to terminate. Unknown Error.")
 
 def force_terminate_process():
     pyautogui.keyDown('alt')
